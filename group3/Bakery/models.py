2b5c067c67457c3243238cebdc03f2c1d60af24d
IVYCharles <charlesvjones17@gmail.com>
2023-07-27 16:13:42 -0500

    Corrections regarding Customer model

--- group3/Bakery/models.py
+++ group3/Bakery/models.py
@@ -1,6 +1,51 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+)
+
+class CustomerManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Email address is required')
+        customer = self.model(email=self.normalize_email(email), **kwargs)
+        customer.set_password(password)
+        customer.save()
+        return customer
+
+    def create_superuser(self, email, password=None, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        return self.create_user(email, password, **kwargs)
+
+class Customer(AbstractBaseUser, PermissionsMixin):
+    id = models.IntegerField(primary_key=True, default=0)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    groups = models.ManyToManyField(Group, blank=True, related_name='customer_set', verbose_name='groups')
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+
+    objects = CustomerManager()
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='customer_user_permissions'
+    )
+
+    def __str__(self):
+        return self.email
+    
+
+
 # SETTER AND GETTER METHODS ARE AUTOMATICALLY CREATED
 # DON'T HAVE TO IMPLEMENT, UNLESS ADDITIONAL FUNCTIONALITY IS NEEDED
 
@@ -9,7 +54,7 @@ class Product_Type(models.Model):
     name = models.CharField(max_length=80, primary_key=True)
 
 class Product(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, default=0)
 
     # Each product can only refer to zero or 1 product type
     product_type = models.OneToOneField(Product_Type, on_delete=models.SET_NULL, null=True)
@@ -18,25 +63,15 @@ class Product(models.Model):
     price = models.FloatField(
         validators=[MinValueValidator(0.0)]
     )
-    
 
-class Customer(models.Model):
-    first_name = models.CharField(max_length=80)
-    last_name = models.CharField(max_length=80)
-    email = models.EmailField(max_length=80, primary_key=True, unique=True)
-    password = models.CharField(max_length=80)
-
-    def getCart():
-        """Returns shopping cart"""
-        pass
 
 
 class Shopping_Cart(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, default=0)
 
     # A shopping car must belong to at most 1 customer
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, db_column='email')
-
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, db_column='customer_id')
+    
     def getTotal(self) -> float:
         # need to query shopping items and sum all collective costs
         # sum([item.getCollectiveCost() for item in self.getItems()])?
